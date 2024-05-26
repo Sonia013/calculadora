1bc1e33e1d0698ab8fa4cb8fde63690d7a36b50a
Sonia013 <4075009@alu.murciaeduca.es>
2024-05-26 19:34:01 +0200

    Subida archivo calculadora.java

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/calculadora.java
@@ -0,0 +1,9 @@
+package com.mycompany.mavenproject1;
+/**
+ * @author Sonia
+ */
+public class calculadora {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
